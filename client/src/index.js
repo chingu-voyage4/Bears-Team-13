1574fc6f4cf270487a11f85b7e6c9baccdca0775
glandon22 <glandonwright@gmail.com>
2018-02-25 20:26:35 -0500

    Add files via upload
    add logic to components

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './components/clock';
+import Greeting from './components/greeting';
+import moment from 'moment';
+import Quote from './components/quote';
+
 
 class App extends Component{
  constructor(props){
-     super(props)
- }  
+     super(props);
+     this.state = {
+         timeOfDay: ''
+     }
+ }
+ 
+ componentWillMount() {
+     //determine what to use in the greeting
+    var currentHour = moment().get('hour');
+    console.log(currentHour);
+    if (currentHour > 4 && currentHour < 12) {
+        this.setState({timeOfDay: 'morning'});
+    }
+
+    else if (currentHour >= 12 && currentHour < 17) {
+        this.setState({timeOfDay: 'afternoon'});
+    }
+    //other times from 5pm to 4am
+    else {
+        this.setState({timeOfDay: 'evening'});
+    }
+ }
+
  render(){
-     return <div>Test</div>
+     //render everything here
+     return (
+     <div>
+        <Clock />
+        <Greeting name={'George'} timeOfDay={this.state.timeOfDay} />
+        <Quote />
+     </div>
+    );
  }
 }
 
