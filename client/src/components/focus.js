3efef705c4afbcf05a00c759410e3e4863df7e50
glandon22 <glandonwright@gmail.com>
2018-02-27 08:08:42 -0500

    updated focus component

--- client/src/components/focus.js
+++ client/src/components/focus.js
@@ -5,21 +5,19 @@ class Focus extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            focus: ''
+            focus: '',
+            hasTodo: false,
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.getNewTodo = this.getNewTodo.bind(this);
+    }
+
+    componentWillMount() {
+        //hit server to check if there is a current focus to preload into component before rendering
     }
 
     getTodo() {
-
-    }
-
-    showTodo() {
-
-    }
-
-    render() {
         return (
             <div className="center-horizontally">
                 <div className="focus-question">
@@ -36,13 +34,49 @@ class Focus extends Component {
         ); 
     }
 
+    showTodo() {
+        return (
+            <div className="current-focus-wrapper">
+                <div className="current-focus">
+                    <div className="today-text">
+                        Today
+                    </div>
+                    <div>
+                        <div className="check-box stack">
+                            <i className="far fa-square"></i>
+                        </div>
+                        <div className="focus-text stack">
+                            {this.state.focus}
+                        </div>
+                        <div className="remove-focus stack" onClick={this.getNewTodo}>
+                            <i className="fas fa-trash-alt"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            this.state.hasTodo ? this.showTodo() : this.getTodo() 
+        );
+    }
+
     onInputChange(focus) {
         this.setState({focus});
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        this.setState({hasTodo: true});
         axios.post('/submitFocus?focus=' + this.state.focus);
+    }
+
+    getNewTodo() {
+        //TODO
+        //call back end to delete the stored focus
+        this.setState({hasTodo: false, focus: ''});
     }
 }
 
