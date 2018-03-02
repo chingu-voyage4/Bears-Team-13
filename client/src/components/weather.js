8767ba439c91dd1a4ebba1a5840b33af756b8635
unknown <glandonwright@gmail.com>
2018-03-02 07:36:02 -0500

    updated weather component

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -17,7 +17,101 @@ class Weather extends Component {
                         </div>
                     </button>
                     <div className="dropdown-menu dropdown-menu-left dropdown-wrapper">
-                        Hello wolrld
+                        <div className="weather-widget-row-small">
+                            <div className="city-holder">
+                                <div className="city-name">
+                                    Miami
+                                </div>
+                                <div className="current-weather-description">
+                                    Clear
+                                </div>
+                            </div>
+                            <div className="more-options-ellipsis-wrapper">
+                                <div className="more-options-ellipsis">
+                                    ...
+                                </div>
+                            </div>    
+                        </div>
+                        <div className="weather-widget-row-large">
+                            <div className="large-weather-icon">
+                                <i className="fas fa-sun"></i>
+                            </div>
+                            <div className="large-current-temperature">
+                                70&deg;
+                            </div>
+                        </div>
+                        <div className="weather-widget-row-small">
+                            <div className="five-day-wrapper active-day">
+                                <div className="day-header">
+                                    FRI
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    <i className="fas fa-bolt"></i>
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    70&deg;
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    65&deg;
+                                </div>
+                            </div>
+                            <div className="five-day-wrapper">
+                                <div className="day-header">
+                                    SAT
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    <i className="fas fa-cloud"></i>
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    70&deg;
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    65&deg;
+                                </div>
+                            </div>
+                            <div className="five-day-wrapper">
+                                <div className="day-header">
+                                    SUN
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    <i className="fas fa-sun"></i>
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    70&deg;
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    65&deg;
+                                </div>
+                            </div>
+                            <div className="five-day-wrapper">
+                                <div className="day-header">
+                                    MON
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    <i className="fas fa-bolt"></i>
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    70&deg;
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    65&deg;
+                                </div>
+                            </div>
+                            <div className="five-day-wrapper">
+                                <div className="day-header">
+                                    TUE
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    <i className="fas fa-cloud"></i>
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    70&deg;
+                                </div>
+                                <div className="five-day-weather-triple">
+                                    65&deg;
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
