d5bfe3094724f9ce7a13b409c62930a5021cd94c
unknown <glandonwright@gmail.com>
2018-03-19 08:05:50 -0400

    Build temp unit conversion method

---
+++ client/src/components/weather-settings.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class WeatherSettings extends Component {
+    constructor(props) {
+        super(props);
+        
+    };
+
+    setC = () => {
+        if (this.props.showFahr) {
+            this.props.changeUnits();
+        }
+
+        else {
+            return;
+        }
+    }
+
+    setF = () => {
+        if (!this.props.showFahr) {
+            this.props.changeUnits();
+        }
+
+        else {
+            return;
+        }
+    }
+    
+    render() {
+        return (
+            <div className="btn-group dropleft">
+                <button type="button" className="btn unstyled-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ...
+                </button>
+                <div className="dropdown-menu weather-settings">
+                    <span className={this.props.showFahr ? '' : 'activeUnits'} onClick={this.setC}>Celsius</span> | <span className={this.props.showFahr ? 'activeUnits' : ''} onClick={this.setF}>Fahrenheit</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WeatherSettings;
