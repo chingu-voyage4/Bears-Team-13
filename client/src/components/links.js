26f0f9327c227b4538895e763593343f346a1ecb
Valerie Seer <seervalerie@gmail.com>
2018-03-22 14:25:49 -0500

    Set event handlers for adding a new link item, updated text shadow

--- client/src/components/links.js
+++ client/src/components/links.js
@@ -4,39 +4,104 @@ class Links extends Component {
     constructor(props){
         super(props);
         this.state = {
-            links: [
-                {
-                    label: '',
-                    url: ''
-                }
-            ]
+            link: {
+                title: '',
+                url: ''
+            }
         };
-        this.handleClick = this.handleClick.bind(this);
+        this.handleWidgetOpen = this.handleWidgetOpen.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleLinkClose = this.handleLinkClose.bind(this);
     }
 
-    handleClick() {
-        var linksPanel = document.getElementById('links-panel');
+    handleWidgetOpen() {
+        let linksPanel = document.getElementById('links-panel');
      
-        // Toggle fade classes  
-        if (linksPanel.className === 'animation-element fadePanelUp') {
-            linksPanel.className = 'animation-element fadePanelDown';
+        // Toggle fade and display classes  
+        if (linksPanel.className === 'animation-element fadePanelUp hide') {
+            linksPanel.className = 'animation-element fadePanelDown show';
         } else {
-            linksPanel.className = 'animation-element fadePanelUp';
+            linksPanel.className = 'animation-element fadePanelUp hide';
+        }
+    }
+
+    handleFocus() {
+        let linksInputTitle = document.getElementById('links-input-title');
+        let linksInputUrl = document.getElementById('links-input-url');
+        let linksCloseBtn = document.getElementById('links-close-btn');
+
+        linksInputTitle.placeholder = 'Name';
+        linksCloseBtn.style.display = 'inline-block';
+        linksInputUrl.style.display = 'block';
+    }
+
+    handleLinkClose() {
+        let linksInputTitle = document.getElementById('links-input-title');
+        let linksInputUrl = document.getElementById('links-input-url');
+        let linksCloseBtn = document.getElementById('links-close-btn');
+
+        linksInputTitle.value = '';
+        linksInputUrl.value = '';
+        linksInputTitle.placeholder = 'New Link';
+        linksCloseBtn.style.display = 'none';
+        linksInputUrl.style.display = 'none';
+    }
+
+    handleSubmit(e) {
+        let linksInputTitle = document.getElementById('links-input-title');
+        let linksInputUrl = document.getElementById('links-input-url');
+        let linksList = document.getElementById('links-list');
+        
+        // If Enter key is pressed    
+        if (e.which === 13 || e.keyCode === 13) { 
+            // If values in both inputs
+            if (linksInputUrl.value != '' && linksInputTitle.value != '') {
+                // Create list item and anchor
+                let linkItem = document.createElement('a');
+
+                linkItem.setAttribute('class', 'link-list-item');
+
+                // Set anchor   
+                linkItem.innerHTML = '<i class="fa fa-globe"></i>' + linksInputTitle.value + '<span class="links-delete-btn animation-element">&times;</span>';
+
+                // Append anchor and list
+                linksList.appendChild(linkItem);
+                
+                let linksCloseBtn = document.getElementsByClassName('links-delete-btn')[0];
+
+                // Display list
+                linksList.style.display = 'block';
+                linkItem.setAttribute('href', `${linksInputUrl}`);
+                linkItem.setAttribute('target', 'blank');
+
+                linksCloseBtn.addEventListener('click', function(){
+                    this.parentNode.parentNode.removeChild(this.parentNode);
+                }) 
+
+                // Reset input fields
+                linksInputTitle.value = '';
+                linksInputUrl.value = '';
+            } else if (linksInputUrl.value === '') {
+                return
+            }
         }
     }
 
     render() {
         return (
             <div className={`links-wrapper ${this.props.visibility}`}>
-                <button id="links-button" onClick={this.handleClick}>Links</button>
-                <ul id="links-panel" className="animation-element fadePanelUp">
-                    <li><a href="chrome-search://local-ntp/local-ntp.html"><i className="fab fa-chrome"></i>Chrome Tab</a></li>
-                    <li><a href="chrome://apps"><i className="fab fa-google"></i>Apps</a></li>
-                    <li><input id="links-input" type="text" placeholder="New Link" /></li>
-                </ul>
+                <button id="links-button" onClick={this.handleWidgetOpen}>Links</button>
+                <div id="links-panel" className="animation-element fadePanelUp hide">
+                    <a href="chrome-search://local-ntp/local-ntp.html"><i className="fa fa-chrome"></i>Chrome Tab</a>
+                    <a href="chrome://apps"><i className="fa fa-google"></i>Apps</a>
+                    <div id="links-list"></div>
+                    <input id="links-input-title" className="links-input" type="text" placeholder="New Link" onFocus={this.handleFocus} onKeyPress={this.handleSubmit} /><span id="links-close-btn" onClick={this.handleLinkClose}>&times;</span>
+                    <input id="links-input-url" className="links-input" type="text" placeholder="URL" onKeyPress={this.handleSubmit} />
+                </div>
             </div>
         )
     }  
 }
-
+// value={this.state.link.title} 
+// value={this.state.link.url}
 export default Links;
