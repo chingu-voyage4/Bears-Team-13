33584054c1f40eea376be6e36bea1ee92dec8201
unknown <glandonwright@gmail.com>
2018-03-29 08:04:50 -0400

    tweaking clock and preparing weather widget for editable location

--- client/src/components/clock.js
+++ client/src/components/clock.js
@@ -22,34 +22,21 @@ class Clock extends Component {
         //take in function from parent passed as prop that updates on click
     }
 
-    // componentWillReceiveProps(nextProps) {
-    //     this.props.militaryTime ? this.setState({time: moment().format('HH:mm')}) : this.setState({time: moment().format('h:mm A')})
-        
-    // }
-
     componentDidMount() {
         var self = this;
-        // determine clock format
-        window.setInterval(function() {
-            self.setState({standardTime:moment().format('h:mm A')})
-            self.setState({miliTime: moment().format('HH:mm')})
-            // if (self.props.militaryTime === true) {
-            //     self.setState({time: moment().format('HH:mm')});
-            // }
-    
-            // else {
-            //     self.setState({time: moment().format('h:mm A')});
-            // }
-        }, 1000)
+        //set state before function to prevent clock render delay
+        self.setState({standardTime:moment().format('h:mm A')});
+        self.setState({miliTime: moment().format('HH:mm')});
+        setInterval(function(){
+            self.setState({standardTime:moment().format('h:mm A')});
+            self.setState({miliTime: moment().format('HH:mm')});
+        }, 1000);
     }
     
-
     render() {
-        let display;
-        this.props.militaryTime ? display = this.state.miliTime : display = this.state.standardTime;
         return (
             <div className="clock center-horizontally" onDoubleClick={this.onDoubleClick}>
-            {display}
+                {this.props.militaryTime ? this.state.miliTime : this.state.standardTime}
             </div>
         )
     }
